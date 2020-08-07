8b755da95dacd054e7d6d4101bdb7d25a8b70e7a
kyle.peng <kyle.peng@ileopard.com>
2020-08-07 11:54:14 +0800

    Initial Commit

---
+++ AppCloseTypeChecker/AppCloseTypeChecker.h
@@ -0,0 +1,19 @@
+//
+//  AppCloseTypeChecker.h
+//  AppCloseTypeChecker
+//
+//  Created by Kyle on 2020/8/7.
+//  Copyright © 2020 Kyle.peng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppCloseTypeChecker.
+FOUNDATION_EXPORT double AppCloseTypeCheckerVersionNumber;
+
+//! Project version string for AppCloseTypeChecker.
+FOUNDATION_EXPORT const unsigned char AppCloseTypeCheckerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppCloseTypeChecker/PublicHeader.h>
+
+
